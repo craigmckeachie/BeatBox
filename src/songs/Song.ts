15c8c345a06622786f14c453b1a6990093d8e138
Craig McKeachie <craigdmckeachie@gmail.com>
2024-08-27 12:11:22 -0400

    created song model

---
+++ src/songs/Song.ts
@@ -0,0 +1,9 @@
+export class Song {
+  id: number | undefined;
+  title: string = "";
+  artist: string = "";
+  album: string = "";
+  year: number | undefined;
+  genre: string = "";
+  durationInSeconds: number | undefined;
+}

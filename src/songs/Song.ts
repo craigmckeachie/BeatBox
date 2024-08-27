7a5d020a72d798c28726954eea31946da222117b
Craig McKeachie <craigdmckeachie@gmail.com>
2024-08-27 12:15:20 -0400

    add song constructor

--- src/songs/Song.ts
+++ src/songs/Song.ts
@@ -6,4 +6,16 @@ export class Song {
   year: number | undefined;
   genre: string = "";
   durationInSeconds: number | undefined;
+
+  constructor(initializer?: any) {
+    if (!initializer) return;
+    if (initializer.id) this.id = initializer.id;
+    if (initializer.title) this.title = initializer.title;
+    if (initializer.artist) this.artist = initializer.artist;
+    if (initializer.album) this.album = initializer.album;
+    if (initializer.genre) this.genre = initializer.genre;
+    if (initializer.year) this.year = initializer.year;
+    if (initializer.durationInSeconds)
+      this.durationInSeconds = initializer.durationInSeconds;
+  }
 }
